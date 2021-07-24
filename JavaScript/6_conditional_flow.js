defa7a6cadd55ba22d0c432cc2db230a41b9e536
Renacin <renacinm@gmail.com>
2021-07-24 10:21:04 -0400

    Conditional ? Operator; Hmmm, it looks useful but kinda tricky

--- JavaScript/6_conditional_flow.js
+++ JavaScript/6_conditional_flow.js
@@ -1,5 +1,5 @@
 // Title: Learning Javascript - Basics: Conditional Flow
-// Date: 23/07/2021
+// Date: 24/07/2021
 // Author: Renacin Matadeen
 //
 //------------------------------------------------------------------------------
@@ -14,18 +14,45 @@
 		+ Programmers use conditional branching to dictate the flow of logic within
 		their code.
 
-	If Branching:
-		+ If a condition is met, the contained code is executed, if it isn't the
-		encapsulated code block is disregarded and code continues
-		+ Code is executed if the condition is true
+	Branching:
+		+ if (){}
+			- If clause is met, the contained code is executed, if it isn't the
+			encapsulated code block is disregarded and code continues
+			- Code is executed if the condition is true. Note, Booleans can take the
+			place of contional statements. Code will always run if true or 1 is in
+			place of condition
+			Ex:
+				+ if (condition) {execute code}
 
-		+ if (condition) {execute code}
+		+ else if () {}
+			- When additional clauses are needed a programmer can make use of the
+			else if conditional branch.
+			- This provides similar functipnality to the if statement
+			Ex:
+				+ else if (condition) {execute code}
+
+		+ else {}
+			- Else clause is met when if, and else if clauses are not met. Can be
+			seen as the fall back statement.
+			- In this clause, no condition is stated.
+			Ex:
+				+ else {execute code}
+
+	Conditional "?" Operator:
+
+
 
 	CAUTION:
 		+ Can add additional functionality within conditions, make use of:
 			- && (And)
 			- || (Or)
 			- !  (Not)
+
+		+ Conditions can have multiple statements, but all must be stored within
+		the first set of parentheses
+
+		Ex:
+			- if (condition_1 && condition_2 || condition_3) {execute code}
 */
 
 //------------------------------------------------------------------------------
