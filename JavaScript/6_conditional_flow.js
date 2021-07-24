7df0ed46bd602acf28c76fafb383d43314253fe7
Renacin <renacinm@gmail.com>
2021-07-24 10:57:47 -0400

    Usage of the conditional operator for variable definition really cleans things up

--- JavaScript/6_conditional_flow.js
+++ JavaScript/6_conditional_flow.js
@@ -7,12 +7,14 @@
    !-- Notes --!
    Following: https://javascript.info/operators
 
+
 	Conditonal Flow:
 		+ All events follow some flow of logic. If this than that; if that
 		then this. Programming takes this proceding and scales it to encompass a
 		wide range of inputs and outputs all.
 		+ Programmers use conditional branching to dictate the flow of logic within
 		their code.
+
 
 	Branching:
 		+ if (){}
@@ -38,9 +40,44 @@
 			Ex:
 				+ else {execute code}
 
+
 	Conditional "?" Operator:
+		+ The content of a variable can also be decided at initialization.
+		+ With the use of the conditional operator a programmer can add branching
+		statements within the definition of a variable
+
+		+ let result = condition ? value1: value2;
+		+ Set the result, either value1, or value2 based on the execution of the
+		condition
+
+		Ex:
+		BEFORE
+			let access_allowed;
+			let age = prompt("How old are you?", "");
+
+			if (age >= 18) {
+				access_allowed = true;
+			}else {
+				access_allowed = false;
+			}
+			alert(access_allowed)
+
+		AFTER
+			let age = prompt("How old are you?", "");
+			let access_allowed = (age >= 18) true: false;
+			let access_allowed = age >= 18
 
 
+	Multiple Conditional "?" Operators:
+		+ When you need to define a variable at initialization with multiple
+		conditons you can also use the ? operator
+
+		Ex:
+			let age = prompt("How old are you?", 18)
+			let welcome_msg = (age <= 3) ? "You are a baby":
+				(age <= 18) ? "You are a Teenager":
+				(age <= 100) ? "You are an Adult":
+				"I have no clue what age that is";
 
 	CAUTION:
 		+ Can add additional functionality within conditions, make use of:
@@ -64,20 +101,13 @@ function greeting_msg(currentDate){
 	let cur_hour = currentDate.getHours()
 
 	// Greeting Between | 00:00 & 11:00 | 12:00 & 17:00 | 18:00 & 24:00
-	if (cur_hour >= 0 && cur_hour <= 11){
-		console.log(`Good Morning: ${cur_time}`)
+	// Much clearner than if, elseif, else blocks
+	let greeting = (cur_hour >= 0 && cur_hour <= 11) ? "Good Morning":
+		(cur_hour >= 12 && cur_hour <= 17) ? "Good Afternoon":
+		(cur_hour >= 18 && cur_hour <= 24) ? "Good Evening":
+		"I have no clue what time it is";
 
-	} else if (cur_hour >= 12 && cur_hour <= 17) {
-		console.log(`Good Afternoon: ${cur_time}`)
-
-	} else if (cur_hour >= 18 && cur_hour <= 24) {
-		console.log(`Good Evening: ${cur_time}`)
-
-	} else {
-		console.log(`No fucking clue what time it is`)
-
-	}
-
+	console.log(`${greeting}, it is currently ${cur_time}`)
 };
 
 //------------------------------------------------------------------------------
