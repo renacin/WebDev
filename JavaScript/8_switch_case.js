420362859f3d0c2401f332ec8666a58580a2fb07
Renacin <renacinm@gmail.com>
2021-07-25 12:02:06 -0400

    learning switch case

---
+++ JavaScript/8_switch_case.js
@@ -0,0 +1,50 @@
+// Title: Learning Javascript - Basics: Switch-Case
+// Date: 25/07/2021
+// Author: Renacin Matadeen
+//
+//------------------------------------------------------------------------------
+/*
+   !-- Notes --!
+   Following: https://javascript.info/switch
+
+	Switch-Case
+		+ Conditional branching can become unwieldy when multiple branches are
+		needed. It's hard to maintain, and track more than 5 branches. One way to
+		solve this is with switch and case branches
+		+ Main benefits of the switch-case methodology is that it can deal with
+		variants of similar outputs
+		+ Cases can be grouped by stacking them together
+		+ The default block acts as a catch all
+
+		+ Basic Syntax:
+			switch (input) {
+				case "match_1":
+					console.log(input)
+
+				case "match_2":
+					console.log(input)
+
+				case "match_3":
+				case "match_4":
+					console.log(input)
+
+				default:
+					console.log("No Clue")
+			}
+
+	CAUTION:
+		+
+*/
+
+//------------------------------------------------------------------------------
+//Create a function that will loop through an array
+function basic_loop(username){
+
+	continue;
+};
+
+//------------------------------------------------------------------------------
+
+// Does JavaScript Have An Entry Point?
+let user_name = prompt("Username: ", "");
+basic_loop(user_name);
