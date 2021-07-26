b16fcc79e879fb73ba813f7e39e0d3260ad671db
Renacin <renacinm@gmail.com>
2021-07-26 10:38:44 -0400

    Functions in javascript

---
+++ JavaScript/9_functions.js
@@ -0,0 +1,26 @@
+// Title: Learning Javascript - Basics: Switch-Case
+// Date: 26/07/2021
+// Author: Renacin Matadeen
+//
+//------------------------------------------------------------------------------
+/*
+   !-- Notes --!
+   Following: https://javascript.info/switch
+
+	Functions:
+		+
+
+	CAUTION:
+		+ You need a break statement in your switch-case tree. Need an exit
+*/
+
+//------------------------------------------------------------------------------
+//Create a function that will loop through an array
+function basic_func(){
+	continue;
+};
+
+//------------------------------------------------------------------------------
+
+// Does JavaScript Have An Entry Point?
+basic_func();
