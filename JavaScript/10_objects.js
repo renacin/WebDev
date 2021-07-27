f88e7d574b0717846173e93aafaf0355a2c5dc9f
Renacin <renacinm@gmail.com>
2021-07-27 16:13:29 -0400

    Focus on objects, not classes

--- JavaScript/10_objects.js
+++ JavaScript/10_objects.js
@@ -12,20 +12,45 @@
 		+ In JavaScript primitive data types contain only one type of variable.
 		+ Objects, break that norm by allowing users to stored key-collections of
 		varing data types.
-		+ Objects are very similar to Python's dictionary data type- Hash Tables?
+		+ Objects are very similar to classes in python. Instances of objects can
+		have properties, as well as methods!
 
 
 	CAUTION:
-		+
+		+ The concept of classes was introduced in JavaScript in ES6 (ECMA2015).
+		However, the concept of true classes does not exist in JavaScript.
+		Use objects as a bridge!
+		+ Javascript has built in objects, like date, and math
 */
 
 //------------------------------------------------------------------------------
 //Create a function that will print the date as a formatted string
-function pass() {
-	return 1;
+function crawlEmpProp(emp) {
+	console.log(`Employee Name: ${emp.firstname}`);
 };
 
 //------------------------------------------------------------------------------
 
 // Does JavaScript Have An Entry Point?
-let x = pass();
+let emp_1 = {
+	firstname: "Bob",
+	lastname: "Ross",
+	email: "big_boss@gmail.com",
+
+	department: "Engineering",
+	salary: 50000
+};
+
+let emp_2 = {
+	firstname: "David",
+	lastname: "Smith",
+	email: "dsmith@gmail.com",
+
+	department: "Management",
+	salary: 80000
+};
+
+let emp_list = [emp_1, emp_2];
+for (let i = 0; i < emp_list.length; i++){
+	crawlEmpProp(emp_list[i]);
+};
