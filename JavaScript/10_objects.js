594d3467576e749d54b761c40cbb1002a57e93a9
Renacin <renacinm@gmail.com>
2021-07-28 12:39:15 -0400

    Object Constructors are fairly similar to Classes in python. Unfortunatly there is some overlap with Dictionaries but I guess it's manageble

--- JavaScript/10_objects.js
+++ JavaScript/10_objects.js
@@ -15,6 +15,26 @@
 		+ Objects are very similar to classes in python. Instances of objects can
 		have properties, as well as methods!
 
+	Object Constructors:
+		+ Instead of manually creating ojects via the object definition method,
+		one can first create a Object Constructor template, and fill in data using
+		that very template.
+
+		+ Remember, in addition to properties, objects can also have methods attached
+		to them
+
+		Basic Syntax:
+		function Template(var1, var2, var3){
+
+			// These are the objects properties
+			this.var1 = var1;
+			this.var2 = var2;
+			this.var3 = var3;
+
+			// These are the methods attached to the objects
+
+		};
+
 
 	CAUTION:
 		+ The concept of classes was introduced in JavaScript in ES6 (ECMA2015).
@@ -24,36 +44,48 @@
 */
 
 //------------------------------------------------------------------------------
-//Create a function that will print the date as a formatted string
-function crawlEmpProp(emp) {
-	console.log(`Employee Name: ${emp["firstname"]}`);
+
+// Create a template that will act as an Object Constructor for User objects
+function User(firstname, lastname, email, department, salary, performance=0.5) {
+
+	// Object Properties
+	this.firstname = firstname;
+	this.lastname = lastname;
+	this.email = email;
+
+	this.department = department;
+	this.salary = salary;
+	this.performance = performance;
 };
+
+// Object Methods
+User.prototype.fullname = function() {
+	return `${this.firstname} ${this.lastname}`;
+};
+
+User.prototype.calcBonus = function() {
+	let bonus = +this.salary * (+this.performance * 0.025);
+	let rounded_bonus = Math.round(bonus)
+	return `${rounded_bonus}`;
+};
+
+
+//Create a function that will print the date as a formatted string
+function displayEmpInfo(emp) {
+	console.log(`Full Name: ${emp.fullname()}, Current Salary: $${emp.salary}, Yearly Bonus: $${emp.calcBonus()}`);
+};
+
 
 //------------------------------------------------------------------------------
 
 // Does JavaScript Have An Entry Point?
-let emp_1 = {
-	firstname: "Bob",
-	lastname: "Ross",
-	email: "big_boss@gmail.com",
+const emp_1 = new User("Bob", "Ross", "bross@gmail.com", "Mechanical", 50000, 0.90);
+const emp_2 = new User("David", "Smith", "dsmith@gmail.com", "IT", 80000, 0.70);
+const emp_3 = new User("Barbara", "Kane", "bkane@gmail.com", "Management", 100000, 0.50);
 
-	department: "Engineering",
-	salary: 50000
-};
+let emp_list = new Array(emp_1, emp_2, emp_3);
 
-let emp_2 = {
-	firstname: "David",
-	lastname: "Smith",
-	email: "dsmith@gmail.com",
-
-	department: "Management",
-	salary: 80000
-};
-
-let emp_list = new Array(emp_1, emp_2);
-
-console.log(`Num Employees: ${emp_list.lenght}`);
-
+// Loop through all employees in array
 for (let i = 0; i < emp_list.length; i++){
-	crawlEmpProp(emp_list[i]);
+	displayEmpInfo(emp_list[i]);
 };
