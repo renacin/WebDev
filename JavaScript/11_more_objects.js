654e5308c61d1db896bfac7e0e3134088859f179
Renacin <renacinm@gmail.com>
2021-08-01 15:48:23 -0400

    Object references are dangerous

--- JavaScript/11_more_objects.js
+++ JavaScript/11_more_objects.js
@@ -13,7 +13,26 @@
 		objects are stored and copied “by reference”, whereas primitive values:
 		strings, numbers, booleans, etc – are always copied “as a whole value”.
 
-		+ In other words, 
+		+ Variables stored in an object store not only the variable, but it's
+		address in memory as well
+
+		+ This is important because when an object variable is copied, the
+		reference is copied, but the object itself is not duplicated. Any changes
+		made to the copy will result in changes to the original
+
+		+ In other words:
+
+			let message_1 = "Hello";
+			message_2 = message_1;
+
+			Here we have two independent variables that have the same value
+
+			let user = {name: 'John'};
+			let admin = user;
+			admin.name = 'Pete';
+			console.log(user.name); || 'Pete'
+
+			When the duplicate is changed so is the original
 
 
 	CAUTION:
