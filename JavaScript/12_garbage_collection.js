9a64406af0e714bf1d879f16c1169988b1eab05f
Renacin <renacinm@gmail.com>
2021-08-05 12:20:19 -0400

    This is very intereting, how does C++ deal with unreachable variables?

--- JavaScript/12_garbage_collection.js
+++ JavaScript/12_garbage_collection.js
@@ -9,7 +9,7 @@
 
 
 	Garbage Collection:
-		+ Put simply, garbage collection (GC) is the process of gaining back memory
+		+ Garbage collection (GC) is the process of gaining back memory
 		that was being allocated to unused/unnneeded variables/objects
 
 		+ Some languages have their own built in GC algorithms, some don't. JavaScript
@@ -18,8 +18,47 @@
 		taken to better understand memory allocation. Both processes are part of
 		the same problem
 
-	Memory Allocation:
-		+
+	Code Reachability
+		+ In JavaScript GC is done automatically; and depends on "reachability".
+		Reachable variables are those that can be easily accessed, they are
+		always stored in memory.
+			Ex: (Root Reachable Variables):
+				- A function and it's local variables / parametres
+				- Global variables
+				- Internal variables
+
+		+ Look at the following syntax:
+
+		Ex - 1:
+			let user = {name: "John"};
+
+			- User is a global variable
+			- Within the user object is a property under the variable name
+
+		Ex - 2:
+			let user = null;
+
+			- Here the user object is written over with a new value. The reference
+			to the original object, and the properties within it are lost.
+			- The property name, and it's value "John" are unreachable; but are
+			stored in memory. The automatic GC script in JavaScript will run and
+			remove unreachable objects
+
+		Ex - 3:
+
+			let user = {name: "John"};
+			let admin = user;
+			let user = null;
+
+			- In this case GC does not run as the original user variable is still
+			referenced by the admin variable.
+			- In order for the original user variable to become unreachable the
+			reference stored within the admin variable must
+
+	Code Reachability: interlinked Objects
+		+ More complex objects require a bit more thought
+
+
 
 	CAUTION:
 		+ Garbage collection helps users write code quicker, more efficiently, and
