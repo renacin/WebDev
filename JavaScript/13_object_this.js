ffda0b96d2366b928ecf17bf7cd7da9d1b172b93
Renacin <renacinm@gmail.com>
2021-08-10 18:37:27 -0400

    Defining methods for single instances versus constructors is a bit different but fairly similar

--- JavaScript/13_object_this.js
+++ JavaScript/13_object_this.js
@@ -13,22 +13,79 @@
 		of templates with object constructors. In this case we will be focusing on
 		single instances of objects.
 
-		+ As a reminder, objects represent 
+		+ Objects store complex sets of information, and are in many cases used to
+		represent real world entities like, orders, shopping carts, etc...
+
+		+ In addition to properties that help dscribe the object, an object can
+		have methods that give it the ability to do things. In other words a 
+		shopping list can have a method that clears all tasks, or charges ones
+		credit card when someone is ready to make a purchase.
+		
+		BASIC SYNTAX:
+			+ Create an instance of an object
+			Once an instance has been created, make an attached method
+			Here we used function expression and attached it to a object
+			property named person_1.sendGreetings
+
+		let person_1 = {
+			name: "Renacin",
+			age: 25,
+			location: "Toronto",
+		};
+
+
+		person_1.sendGreetings = function () {
+			console.log("Hello");
+		};
+
+
+	Method Shorthand
+		+ We just learned how to create methods, and link functions to objects
+		before we move on however we must cover the shorthand notation that makes
+		method creation much quicker and easier
+
+		BASIC SYNTAX:
+			+ Store everything within the single instance of the object
+			+ Since everything is stored within the object instance make reference
+			to the object, and them the variable with "this.property"
+			+ Don't add a semicolon at the end of the function as it ends the object early
+
+
+		let person_1 = {
+			name: "Renacin",
+			age: 25,
+			location: "Toronto",
+
+			sendGreetings () {
+				console.log(`Hey my name is ${this.name});
+			}
+		};
 
 	CAUTION:
-		+
+		+ You can assign predeeclared functions to objects, 
+		just create the linked method name (object.method_name) and equate
+		it to the function that was already declared
+
+
 */
 
 
 //------------------------------------------------------------------------------
 
-//Create a basic function
-function function_1() {
-	console.log("Hello");
+//Create a basic function	
+let person_1 = {
+	name: "Renacin",
+	age: 25,
+	location: "Toronto",
+
+	sendGreetings () {
+		console.log(`Hello my name is ${this.name}, and I currently live in ${this.location}`);
+	}
 };
+
 
 
 //------------------------------------------------------------------------------
 
 // Does JavaScript Have An Entry Point?
-function_1();
+person_1.sendGreetings();
