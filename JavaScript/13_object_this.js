11a74e98c5b863428741ccab9ef406d009ce6b58
Renacin <renacinm@gmail.com>
2021-08-10 12:26:43 -0400

    Using THIS keyword with objects

---
+++ JavaScript/13_object_this.js
@@ -0,0 +1,29 @@
+// Title: Learning Javascript - Basics: Objects | This
+// Date: 10/08/2021
+// Author: Renacin Matadeen
+//
+//------------------------------------------------------------------------------
+/*
+   !-- Notes --!
+   Following: https://javascript.info/object-methods
+
+
+	Using "This" With Objects
+
+	CAUTION:
+		+
+*/
+
+
+//------------------------------------------------------------------------------
+
+//Create a basic function
+function function_1() {
+	console.log("Hello");
+};
+
+
+//------------------------------------------------------------------------------
+
+// Does JavaScript Have An Entry Point?
+function_1();
