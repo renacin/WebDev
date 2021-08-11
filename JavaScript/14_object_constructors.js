dd03643a05cc24722fff12e4a22233bb703f9c64
Renacin <renacinm@gmail.com>
2021-08-11 14:14:26 -0400

    Mov onto Object Constructors

---
+++ JavaScript/14_object_constructors.js
@@ -0,0 +1,34 @@
+// Title: Learning Javascript - Basics: Objects | Constructors
+// Date: 11/08/2021
+// Author: Renacin Matadeen
+//
+//-----------------------------------------------------------------------------
+/*
+   !-- Notes --!
+   Following: https://javascript.info/constructor-new
+
+
+	Object Constuctors:
+        + 
+
+	CAUTION:
+		+ 
+
+
+*/
+
+
+//-----------------------------------------------------------------------------
+
+// Create a basic function that shows the movement of a character on a
+// cartesian plane
+
+let character = {
+	name: "Renacin",
+};
+
+
+
+//------------------------------------------------------------------------------
+
+// Use Object Constructors
