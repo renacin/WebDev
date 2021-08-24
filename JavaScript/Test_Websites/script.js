acc53aec3720ec7e1870c87c976d56c66e852a7e
Renacin <renacinm@gmail.com>
2021-08-23 21:09:57 -0400

    Make a website that takes user input and returns certain outputs

---
+++ JavaScript/Test_Websites/script.js
@@ -0,0 +1,10 @@
+// Title: Learning Javascript - Sorting A List Of User Inputted Numbers
+// Date: 15/08/2021
+// Author: Renacin Matadeen
+//
+//-----------------------------------------------------------------------------
+// Create a constructor function that acts as a template for students
+
+
+//------------------------------------------------------------------------------
+// Use Object Constructors
