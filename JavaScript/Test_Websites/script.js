7157a1972e409cd12cc3821986063ed1335f15c7
Renacin <renacinm@gmail.com>
2021-09-17 10:04:28 -0400

    Major events really do screw up good habits

--- JavaScript/Test_Websites/script.js
+++ JavaScript/Test_Websites/script.js
@@ -1,4 +1,5 @@
 // Title: Learning Javascript - Sorting A List Of User Inputted Numbers
+//                              Getting back into the swing of things
 // Date: 24/08/2021
 // Author: Renacin Matadeen
 //
